2f96a0dbcf5e048f6bc6f48df85b4f91afa62499
Andres Alban <bethesdas101@gmail.com>
2020-10-25 17:30:23 -0500

    Updating HTML user presentation component

--- src/components/user-presentation/user-presentation.component.jsx
+++ src/components/user-presentation/user-presentation.component.jsx
@@ -3,28 +3,26 @@ import { connect } from 'react-redux';
 
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectTodoTasks } from '../../redux/todoList/todoList.selectors';
-import Welcome from '../welcome/welcome.component';
-
 
 import './user-presentation.styles.scss';
+import TaskForm from '../task-form/task-form.component';
 
-const UserPresentation = ({ currentUser, todoList }) => {                
+const UserPresentation = ({ currentUser, todoList }) => { 
     return (        
-        currentUser ? 
         <div className='info-container'>        
-            <div className='image'>
-                <img src={currentUser.currentUser.photoURL} alt='profile' className='profile'/>
-            </div>
-            <div className='user-info'>
-                <h1>Welcome</h1>
-                <h2>{currentUser.currentUser.displayName}</h2>
-                <p>You have <span className='task-counter'>{todoList.todoList.length}</span> task(s) for today</p>
-            </div>
-        </div> : 
-        <div className='welcome'>
-            <Welcome />
-        </div>       
-    )
+            <div className='user-container'>
+                <div className='image'>
+                    <img src={currentUser.currentUser.photoURL} alt='profile' className='profile'/>
+                </div>
+                <div className='user-info'>
+                    <h1>Welcome</h1>
+                    <h2>{currentUser.currentUser.displayName}</h2>
+                    <p>You have <span className='task-counter'>{todoList.length}</span> task(s) for today</p>
+                </div>
+            </div>            
+            <TaskForm />            
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
